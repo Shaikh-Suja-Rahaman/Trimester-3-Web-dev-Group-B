c806439a82af5e87f0c28f1b06208a8d4fdcae84
mrinal1224 <iammrinalkc@gmail.com>
2025-02-03 11:09:33 +0530

    push

--- Class-1 (Async-Await)/coffee.js
+++ Class-1 (Async-Await)/coffee.js
@@ -1,15 +1,41 @@
 function placeOrder(drink) {
-    return new Promise(function (resolve, reject) {
-      if (drink === "coffee") {
-        resolve("Order for Coffee Placed.");
-      } else {
-        reject("Order can not be Placed.");
-      }
-    });
+  return new Promise(function (resolve, reject) {
+    if (drink === "coffee") {
+      resolve("Order for Coffee Placed.");
+    } else {
+      reject("Order can not be Placed.");
+    }
+  });
+}
+
+function processOrder(orderPlaced) {
+  return new Promise(function (resolve) {
+    resolve(`${orderPlaced} and Served.`);
+  });
+}
+
+//   Resolve it with using Promises
+
+// placeOrder('coffee').then(function(orderPlaced){
+//   console.log(orderPlaced)
+//   return processOrder(orderPlaced)
+// }).then(function(proccessedOrder){
+//     console.log(proccessedOrder)
+// }).catch(function(orderPlaced){
+//    console.log(orderPlaced)
+// })
+
+// Async Await
+async function serveOrder() {
+  try {
+    let data1 = await placeOrder("coffee");
+    console.log(data1);
+
+    let data2 = await processOrder(data1);
+    console.log(data2);
+  } catch (error) {
+    console.log(error);
   }
-  
-  function processOrder(orderPlaced) {
-    return new Promise(function (resolve) {
-      resolve(`${orderPlaced} and Served.`);
-    });
-  }
+}
+
+serveOrder();
