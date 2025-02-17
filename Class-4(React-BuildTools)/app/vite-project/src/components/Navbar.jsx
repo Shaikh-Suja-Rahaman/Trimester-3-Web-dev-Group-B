d3cfe367120fec3ee1431cdbe7263e8b8e042ce1
mrinal1224 <iammrinalkc@gmail.com>
2025-02-17 11:35:15 +0530

    push

---
+++ Class-4(React-BuildTools)/app/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">Amazon</div>
+      <div className="navbar-search">
+        <input
+          type="text"
+          placeholder="Search for products"
+          className="search-input"
+        />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="navbar-links">
+        <a href="#deals" className="nav-link">
+          Today's Deals
+        </a>
+        <a href="#cart" className="nav-link">
+          Cart
+        </a>
+
+        <button className="nav-login-button">
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
