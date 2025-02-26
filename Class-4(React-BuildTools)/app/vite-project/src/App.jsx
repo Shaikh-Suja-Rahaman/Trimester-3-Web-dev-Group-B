2ddbaff8062e4fb4db3fdd0f33024fc7818871b4
mrinal1224 <iammrinalkc@gmail.com>
2025-02-26 10:50:45 +0530

    push

--- Class-4(React-BuildTools)/app/vite-project/src/App.jsx
+++ Class-4(React-BuildTools)/app/vite-project/src/App.jsx
@@ -3,15 +3,18 @@ import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Counter from "./components/Counter";
 import Form from "./components/Form";
+import Ue from "./components/Ue";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Card name='Apple' description='this is a macbook 2018'/>
       <Card name='Samsung' description='S24'/>
       <Card name='Nike Shoes' description='Air Jordan'/>
-      <Card name='Sunglasses' description='These are Rayban sunglasses'/>
+      <Card name='Sunglasses' description='These are Rayban sunglasses'/> */}
+
+      <Ue/>
 
     
     </div>
---
+++ Class-4(React-BuildTools)/app/vite-project/src/components/Ue.jsx
@@ -0,0 +1,47 @@
+import React, { use, useEffect, useState } from "react";
+
+function Ue() {
+  const [count, setCount] = useState(0);
+  const [text , setText] = useState('')
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+//   useEffect(()=>{
+//     console.log('Use effect Runs')
+//     document.title = `Button Clicked ${count} times`
+//   })
+
+//   useEffect(()=>{
+//     console.log('Use effect Runs')
+//     document.title = `Button Clicked ${count} times`
+//   }, [])
+
+  useEffect(()=>{
+    console.log('Use effect Runs')
+    document.title = `Button Clicked ${count} times`
+  }, [text])
+  
+  // useEffect hook runs at the end 
+  
+
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={decrement}>Decrement</button>
+
+      <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+      <h3>{text}</h3>
+    </div>
+  );
+}
+
+export default Ue;
