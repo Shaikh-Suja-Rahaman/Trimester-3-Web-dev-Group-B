c6dcd09dade54d506186887f8b830cd3418adfb7
mrinal1224 <iammrinalkc@gmail.com>
2025-03-05 11:49:59 +0530

    push

--- Class-7/IMDB/vite-project/src/components/Movies.jsx
+++ Class-7/IMDB/vite-project/src/components/Movies.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieCard from "./MovieCard";
+import axios from "axios";
 
 function Movies() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=1"
+      )
+      .then(function (response) {
+        console.log(response.data.results);
+      })
+      .catch(function (err) {
+        console.log("Cannot call TMDB API ", err);
+      });
+  }, []);
+
   return (
     <div>
       <div className="text-2xl font-bold text-center m-5">
@@ -9,14 +23,6 @@ function Movies() {
       </div>
       <div className="flex justify-evenly flex-wrap gap-8 ">
         <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-       
       </div>
     </div>
   );
