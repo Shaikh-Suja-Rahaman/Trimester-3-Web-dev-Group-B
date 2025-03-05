6362b37f384b78568635b32f2543f4c622058652
mrinal1224 <iammrinalkc@gmail.com>
2025-03-05 11:08:41 +0530

    push

--- Class-7/IMDB/vite-project/src/App.jsx
+++ Class-7/IMDB/vite-project/src/App.jsx
@@ -4,6 +4,7 @@ import Movies from "./components/Movies";
 import Navbar from "./components/Navbar";
 import WatchList from "./components/WatchList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Banner from "./components/Banner";
 function App() {
   return (
     <>
@@ -11,7 +12,14 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Movies />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner /> <Movies />
+              </>
+            }
+          />
           <Route path="/watchlist" element={<WatchList />} />
           <Route path="/recommend" element={<MovieRecommendation />} />
         </Routes>
--- Class-7/IMDB/vite-project/src/components/Movies.jsx
+++ Class-7/IMDB/vite-project/src/components/Movies.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
+import MovieCard from "./MovieCard";
 
 function Movies() {
   return (
-    <div
-      className="h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end"
-      style={{
-        backgroundImage:
-          ``,
-      }}
-    >
-      <div className="text-white w-full text-center text-2xl">
-        Avengers Endgame
+    <div>
+      <div className="text-2xl font-bold text-center m-5">
+        <h1>Trending Movies</h1>
+      </div>
+      <div className="flex justify-evenly flex-wrap gap-8 ">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
       </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
