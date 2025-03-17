7d34481ecb101593539496f24d5f2f3d8d54129d
mrinal1224 <iammrinalkc@gmail.com>
2025-03-17 10:42:51 +0530

    push

--- Class-7/IMDB/vite-project/src/components/Movies.jsx
+++ Class-7/IMDB/vite-project/src/components/Movies.jsx
@@ -1,15 +1,30 @@
 import React, { use, useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import axios from "axios";
+import Pagination from "./Pagination";
+
 
 function Movies() {
   const [movies , setMovies] = useState(null)
   const [loading , setLoading] = useState(true)
+  const [page , setPage] = useState(1)
+
+
+  const pageNext = ()=>{
+     setPage(page+1)
+  }
+
+  const pagePrev = ()=>{
+    if(page>1){
+      setPage(page-1)
+    }
+
+  }
 
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=1"
+        `https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=${page}`
       )
       .then(function (response) {
         console.log(response.data.results);
@@ -20,7 +35,7 @@ function Movies() {
       .catch(function (err) {
         console.log("Cannot call TMDB API ", err);
       });
-  }, []);
+  }, [page]);
 
 
 
@@ -35,6 +50,7 @@ function Movies() {
         <MovieCard movieObj={movieObj}/>
        ))}
       </div>
+     <Pagination pageNext={pageNext} pagePrev={pagePrev} pageNo={page}/>
     </div>
   );
 }
